0a02e3448dfadc039b5de9c0cea1bcb501be23b7
Long-Soeng168 <generalpurpose167@gmail.com>
2025-03-06 18:12:30 +0700

    update

--- app/Livewire/SlideCreate.php
+++ app/Livewire/SlideCreate.php
@@ -54,14 +54,14 @@ class SlideCreate extends Component
 
         // dd($validated);
 
-        if(!empty($this->image)){
+        if (!empty($this->image)) {
             // $filename = time() . '_' . $this->image->getClientOriginalName();
             $filename = time() . str()->random(10) . '.' . $this->image->getClientOriginalExtension();
 
-            $image_path = public_path('assets/images/slides/'.$filename);
-            $image_thumb_path = public_path('assets/images/slides/thumb/'.$filename);
+            $image_path = public_path('assets/images/slides/' . $filename);
+            $image_thumb_path = public_path('assets/images/slides/thumb/' . $filename);
             $imageUpload = Image::make($this->image->getRealPath())->save($image_path);
-            $imageUpload->resize(1280,null,function($resize){
+            $imageUpload->resize(900, null, function ($resize) {
                 $resize->aspectRatio();
             })->save($image_thumb_path);
             $validated['image'] = $filename;
